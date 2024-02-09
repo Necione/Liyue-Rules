f52ce5ed1e20be8ca544efec562bccfa6060fc91
Necione <69877102+Necione@users.noreply.github.com>
2024-02-08 23:06:55 -0800

    fix: leaks allowed

--- public/rules.js
+++ public/rules.js
@@ -2,7 +2,7 @@ const rules = [
     { id: "A1", rule: "Follow Discord TOS, Terms of Service and Guidelines", punishment: "Immediate ban with no chance of appeal", severity: "red" },
     { id: "A1-1", rule: "DM advertising any external Discord Server, paid service, or for commissions is prohibited", punishment: "Immediate ban with little chance of appeal", severity: "red" },
     { id: "A2", rule: "Follow all HoYoverse TOS", punishment: "Immediate ban with no chance of appeal", severity: "red" },
-    { id: "A2-1", rule: "Do not discuss any Genshin Impact, Honkai: Star Rail or other HoYoverse game leaks", punishment: "Immediate ban with no chance of appeal", severity: "red" },
+    { id: "A2-1", rule: "Clarification: Discussion of leaks are ALLOWED, but make sure to use spoilers", punishment: "N/A", severity: "green" },
     { id: "A3", rule: "The mention/discussion of breaking said either Genshin or Discord TOS (including, but not limited to, selling Genshin Impact accounts, Discord Tokens, etc.) is also prohibited", punishment: "User will be flagged with possible mute", severity: "orange" },
     { id: "B1", rule: "You are not allowed to spam, flood, or send random messages in any channel with the exception of Mudae-related channels & bot commands", punishment: "User will be flagged with possible mute", severity: "green" },
     { id: "B2", rule: "You are not allowed to send any content containing pictures of nudity, sex, pornography, anything that is 'see-through'", punishment: "User will be muted for an extended time. Ban with low chance of appeal is possible", severity: "red" },
